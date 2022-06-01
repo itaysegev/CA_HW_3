0f048e0b807bb286ba6fd13152ecf3fc05b2407d
Itay Segev <itaysegev11@gmail.com>
2022-06-01 12:37:27 +0300

    graph build

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -7,13 +7,55 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <map>
+#include <list>
 
 using namespace std;
-ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) {
-    cout << "AAAAAAAAA" << endl;
-    for(int i = 0; i < numOfInsts; i++){
-        cout << progTrace[i].opcode << endl;
+
+class Graph
+{
+    map<InstInfo, list <pair <InstInfo, int>>> l; // adjacency list
+public:
+    void add_edge(InstInfo node, InstInfo neighbour, int distance) {
+        l[node].push_back(make_pair(neighbour, distance));
     }
+ 
+    void print_graph()
+    {
+        // now we will iterate over all the keys in the map
+        // then we will print the linked list of neighbors
+        // associated with these nodes
+        for(auto p : l)
+        {
+            // iterate over all the neighbors of this particular node
+            InstInfo node = p.first;
+            list <pair <InstInfo, int>> neighbour = p.second;
+ 
+            cout << "Neighbors of: " << node.opcode << " are:\n";
+ 
+            for(auto nbr : neighbour)
+            {
+                InstInfo dest = nbr.first;
+                int distance = nbr.second;
+ 
+                cout << "Neighbour: " << dest.opcode << " " << " Distance: "<< distance << endl;
+            }
+            cout << endl;
+        }
+    }
+};
+
+ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) {
+    Graph g;
+    InstInfo entry, exit;
+    g.add_edge(entry, progTrace[0], 0);
+    int i;
+    for(i = 0; i < numOfInsts - 1 ; i++){
+        g.add_edge(progTrace[i], progTrace[i+1], 0);
+
+    }
+    g.add_edge(progTrace[i], exit, 0);
+    g.print_graph();
     return PROG_CTX_NULL;
 }
 
