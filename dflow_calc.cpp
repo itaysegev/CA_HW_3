5ff8532b991abf22285df31d07e6f6237c3c6841
Itay Segev <itaysegev11@gmail.com>
2022-06-03 13:40:47 +0300

    getInstDepth func

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -257,9 +257,9 @@ int getInstDepth(ProgCtx ctx, unsigned int theInst) {
     Graph g(n);
     int dist[g.n], prev[g.n];
     int start = 2;
-    g.addEdge(1, 0, 1);
-    g.addEdge(2, 1, 1);
-    g.addEdge(3, 2, 1);
+    g.addEdge(1, 2, 1);
+    g.addEdge(0, 1, 1);
+    // g.addEdge(3, 2, 1);
     // g.addEdge(3, 0, 4);
     // g.addEdge(4, 0, 2);
     // g.addEdge(4, 1, 2);
