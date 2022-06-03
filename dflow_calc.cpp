7148afe8b5d6542c96bb535bda8e5168913eadd5
Itay Segev <itaysegev11@gmail.com>
2022-06-03 16:19:29 +0300

    getProgDepth func

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -173,13 +173,14 @@ ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[],
 }
 
 void freeProgCtx(ProgCtx ctx) {
+
 }
 
 int getInstDepth(ProgCtx ctx, unsigned int theInst) {
     Graph g = *(Graph*)ctx;
     int dist[g.n];
-    g.DijkstraAlgo(dist, theInst);
-    return  (-1 * dist[g.entry_index]);
+    g.DijkstraAlgo(dist, g.exit_index);
+    return  (-1 * dist[theInst]);
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
@@ -194,6 +195,8 @@ int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2De
 }
 
 int getProgDepth(ProgCtx ctx) {
+    Graph g = *(Graph*)ctx;
+    g.printMat();
     int exit = (*(Graph*)ctx).exit_index;
     return getInstDepth(ctx, exit);
 }
