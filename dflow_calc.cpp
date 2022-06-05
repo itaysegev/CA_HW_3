d8f88a28517d5ac3bd761cc2b5fc562c3cd9133a
Itay Segev <itaysegev11@gmail.com>
2022-06-05 15:02:56 +0300

    tests

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -262,7 +262,7 @@ int getProgDepth(ProgCtx ctx) {
     int exit = (*g).exit_index;
     // int dist[(*g).V];
     // (*g).longestPath(exit, dist);
-    // return getInstDepth(ctx, exit);
+    return getInstDepth(ctx, exit);
     // cout << getInstDepth(ctx, 3071) << endl;
     // cout << getInstDepth(ctx, 9) << endl;
     // cout << getInstDepth(ctx, 11) << endl;
@@ -283,5 +283,5 @@ int getProgDepth(ProgCtx ctx) {
     //         max_idx = i;
     //     }
     // }
-    return 0;
+    // return 0;
 }
